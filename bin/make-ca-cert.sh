9b786197b915132c436b9b84ddda34ad0b9b0ef9
Timo Lindemann <lindem@tilin.de>
2014-03-13 15:12:02 +0100

    made make-ca-cert.sh use the functionality in functions.sh

--- bin/make-ca-cert.sh
+++ bin/make-ca-cert.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/functions.sh
 
-# if the location of the openssl binary is defined in the OPENSSL envvar, prefer
-# that. If not, try to locate it with which.
-if [[ -z "${OPENSSL+x}" ]]
-then
-    OPENSSL=`which openssl`
-fi
+find_openssl
 
-# if $OPENSSL is executable, everything is fine. If not, exit here.
-if [[ ! -x $OPENSSL ]]
+if [[ $? -ne 0 ]]
 then
     echo "the openssl command was not found. You can supply its location with"
     echo "the environment variable OPENSSL"
     exit 1;
 fi
 
-if [[ -e ca/ca.cnf && -e ca/private && -e ca/cert ]]
+# check for the necessary files.
+check_files ca/ca.cnf \
+    ca/private \
+    ca/cert
+
+if [[ $? -eq 0 ]]
 then
     echo "making certificate using your ca/ca.cnf. Ctrl+C to abort, enter to"
     echo "proceed."
     read
-    openssl req -new -x509 -nodes -days 3650 -config ca/ca.cnf -keyform PEM \
+    $OPENSSL req -new -x509 -nodes -days 3650 -config ca/ca.cnf -keyform PEM \
         -keyout ca/private/cakey.pem -outform PEM -out ca/cert/cacert.pem
+    $OPENSSL x509 -in ca/cert/cacert.pem -outform der -out ca/cert/cacert-windows.crt
     if [ $? -ne 0 ]
     then
         echo "---> An error occured."
     else
+        echo
+        echo "you can now use make-new-cert.sh to create a signed certificate."
         echo "---> All done."
     fi
-
 else
     echo "the folder structure was not found. Please run this script in the"
     echo "directory where you ran make-ca-folders.sh."
